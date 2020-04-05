e12a309452c8b56162c00fbf494391410dd536ed
Joan Reyero <joanreyero@gmail.com>
2020-04-05 18:17:21 +0100

    Changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'The Star Gift',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Star gifts handpicked for you',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
